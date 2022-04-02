4e97ca59a814a2cd1ebf6dc15504576d58286946
Kotonovsky <kotonovsky@bk.ru>
2022-04-02 16:44:06 +0300

    11/36 lessons_part_02 (subs/ blogger..)

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.myfirstapp
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import android.view.View
-import android.widget.ActionMenuView
-import android.widget.TextView
-import android.widget.Button
+import com.example.myfirstapp.constance.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 
@@ -22,28 +17,53 @@ class MainActivity : AppCompatActivity() {
 
         bindingClass.btResult.setOnClickListener {
 
-            val resultValue = bindingClass.edValue.text.toString().toInt()
+            val resultValue = bindingClass.edValue.text.toString()
 
             when (resultValue){
 
-                in 0..999 -> {
+                Constance.ENGINEER -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "New blogger"
+                    val tempText = "Income: ${Constance.ENGINEER_INCOME}"
+
+                    if (bindingClass.edPassword.text.toString() == Constance.ENGINEER_PASSWORD) {
+                        bindingClass.tvResult.text = tempText
+                    } else {
+                        bindingClass.tvResult.text = "Wrong PASS"
+                    }
                 }
 
-                in 1000..100000 -> {
+                Constance.CLEANER -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Medium blogger"
+                    val tempText = "Income: ${Constance.CLEANER_INCOME}"
+
+                    bindingClass.tvResult.text =
+                        if (bindingClass.edPassword.text.toString() == Constance.CLEANER_PASSWORD) {
+                            tempText
+                    } else {
+                            "Wrong PASS"
+                    }
+                }
+
+                Constance.DIRECTOR -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    val tempText = "Income: ${Constance.DIRECTOR_INCOME}"
+
+                    bindingClass.tvResult.text =
+                        if (bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD) {
+                            tempText
+                        } else {
+                            "Wrong PASS"
+                        }
                 }
 
                 else -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Top blogger"
+                    bindingClass.tvResult.text = "Who is this?"
                 }
             }
 
         }
 
-        }
+    }
 
 }
---
+++ app/src/main/java/com/example/myfirstapp/constance/Constance.kt
@@ -0,0 +1,16 @@
+package com.example.myfirstapp.constance
+
+object Constance {
+
+        const val ENGINEER_INCOME = 10000
+        const val CLEANER_INCOME = 1000
+        const val DIRECTOR_INCOME = 100000
+
+        const val DIRECTOR = "Egor"
+        const val ENGINEER = "Andrey"
+        const val CLEANER = "Sergey"
+
+        const val DIRECTOR_PASSWORD = "4565"
+        const val ENGINEER_PASSWORD = "1232"
+        const val CLEANER_PASSWORD = "6677"
+}
