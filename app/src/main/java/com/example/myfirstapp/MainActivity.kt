f3295ff8aa16fa532ec6323580bf4080b2a444ab
Kotonovsky <kotonovsky@bk.ru>
2022-04-02 15:26:53 +0300

    11/36 lessons (subs/ blogger..)

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -13,37 +14,36 @@ import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass : ActivityMainBinding
-    val maxPerson = 90
-    val currentPerson = 35
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
+        bindingClass.btResult.setOnClickListener {
 
-        bindingClass.b1.setOnClickListener {
+            val resultValue = bindingClass.edValue.text.toString().toInt()
 
-            bindingClass.txResult.text =
-                if(maxPerson > currentPerson)
-                    "All is good"
-                else
-                    "Full limit"
+            when (resultValue){
+
+                in 0..999 -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "New blogger"
+                }
+
+                in 1000..100000 -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "Medium blogger"
+                }
+
+                else -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    bindingClass.tvResult.text = "Top blogger"
+                }
+            }
 
         }
 
-        bindingClass.b2.setOnClickListener {
-
-
         }
-
-        bindingClass.b3.setOnClickListener {
-
-
-        }
-
-    }
-
-
 
 }
