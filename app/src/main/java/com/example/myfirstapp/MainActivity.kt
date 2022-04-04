d5e893f47d2bad8d9d29f4c91fe021136798ebbe
Kotonovsky <kotonovsky@bk.ru>
2022-04-04 10:19:04 +0300

    13/36 Done. Moving on

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.myfirstapp.constance.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
@@ -17,48 +18,73 @@ class MainActivity : AppCompatActivity() {
 
         bindingClass.btResult.setOnClickListener {
 
+            Log.d("MyLog", "ID dula = ${R.drawable.dula}")
+
             val resultValue = bindingClass.edValue.text.toString()
+            bindingClass.imPhoto.visibility = View.VISIBLE
 
             when (resultValue){
 
                 Constance.ENGINEER -> {
+
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Income: ${Constance.ENGINEER_INCOME}"
 
                     if (bindingClass.edPassword.text.toString() == Constance.ENGINEER_PASSWORD) {
+
                         bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.andrey)
+
                     } else {
+
                         bindingClass.tvResult.text = "Wrong PASS"
+                        bindingClass.imPhoto.setImageResource(R.drawable.dula)
+
                     }
                 }
 
                 Constance.CLEANER -> {
+
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Income: ${Constance.CLEANER_INCOME}"
 
-                    bindingClass.tvResult.text =
-                        if (bindingClass.edPassword.text.toString() == Constance.CLEANER_PASSWORD) {
-                            tempText
+                    if (bindingClass.edPassword.text.toString() == Constance.CLEANER_PASSWORD) {
+
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.sergey)
+
                     } else {
-                            "Wrong PASS"
+
+                        bindingClass.tvResult.text = "Wrong PASS"
+                        bindingClass.imPhoto.setImageResource(R.drawable.dula)
+
                     }
                 }
 
                 Constance.DIRECTOR -> {
+
                     bindingClass.tvResult.visibility = View.VISIBLE
                     val tempText = "Income: ${Constance.DIRECTOR_INCOME}"
 
-                    bindingClass.tvResult.text =
-                        if (bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD) {
-                            tempText
+                    if (bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD) {
+
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.egor)
+
                         } else {
-                            "Wrong PASS"
+
+                        bindingClass.tvResult.text = "Wrong PASS"
+                        bindingClass.imPhoto.setImageResource(R.drawable.dula)
+
                         }
                 }
 
                 else -> {
+
                     bindingClass.tvResult.visibility = View.VISIBLE
                     bindingClass.tvResult.text = "Who is this?"
+                    bindingClass.imPhoto.setImageResource(R.drawable.dula)
+
                 }
             }
 
